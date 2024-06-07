74b23c9862420dd8ad562c9cf01fdd122f9bb296
JongD <JongD@JONGDO>
2024-06-06 22:26:16 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment5._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter individual digits to calculate the sum: ");
+            string num = Console.ReadLine();
+
+            Console.WriteLine(SumofNum(num));
+        }
+        static int SumofNum(string num)
+        {
+            int sum = 0; 
+            for (int i = 0; i < num.Length; i++)
+            {
+                int numConverted = int.Parse(num[i].ToString());   //the int.Parse(...) function is used to convert the resulting string back to an integer. 
+
+                sum += numConverted;
+
+            }
+            return sum;
+        }
+     
+        
+
+        
+    }
+}   
